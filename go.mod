25a5fe777611122bf10c2c68971aeca2da3a2319
Pedro Alvarenga Afonso <p.alvarenga.dev@gmail.com>
2026-03-09 10:17:03 -0300

    Reset repository for new simulator architecture

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/p-alvarenga/go_tcp-tracker-simulator
-
-go 1.25.6
--- internal/domain/device/device_lbs.go
+++
@@ -1,8 +0,0 @@
-package device
-
-type lbs struct {
-	MCC    uint16 // Mobile Country Code
-	MNC    uint8  // Mobile Network Code
-	LAC    uint16 // Location Area Code
-	CellID uint32
-}
